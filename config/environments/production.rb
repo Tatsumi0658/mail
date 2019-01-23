1983538ea2926a60c856f059aa468f51d2111c61
koda <hogehogehoge@gmail.com>
2019-01-23 21:14:12 +0900

    firstcommit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host:"polar-waters-91686.herokuapp.com/" }
+  config.action_mailer.default_url_options = { host:"fierce-wildwood-56530.herokuapp.com" }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
