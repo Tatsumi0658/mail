9a705e2b5307896705d500c653fbae1991db79fe
koda <hogehogehoge@gmail.com>
2019-01-29 01:27:19 +0900

    debugレベルの変更

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,9 @@ module Mail
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
 
+    config.log_level = :info
 
-    #config.i18n.default_locale = :ja
+    config.i18n.default_locale = :ja
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
