42eb6d31b5dd1dda50f1f7fc3bb566a6de140527
koda <hogehogehoge@gmail.com>
2019-01-29 01:18:12 +0900

    お問い合わせメールの宛先変更

--- app/mailers/contact_mail_mailer.rb
+++ app/mailers/contact_mail_mailer.rb
@@ -2,6 +2,6 @@ class ContactMailMailer < ApplicationMailer
   def contact_mail(contact)
     @contact = contact
 
-    mail to:"自分のアドレス", subject:"お問い合わせの確認メール"
+    mail to:"1ec183852a@gmail.com", subject:"お問い合わせの確認メール"
   end
 end
