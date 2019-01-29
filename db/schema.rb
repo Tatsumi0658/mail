0eb48b8a46856215a456d4dcfb9e578b24f257f1
koda <hogehogehoge@gmail.com>
2019-01-29 21:22:01 +0900

    Emailにuniqueを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_26_061244) do
+ActiveRecord::Schema.define(version: 2019_01_29_122016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2019_01_26_061244) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "posts", "profiles"
