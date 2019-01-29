bd5eb65089b44adf17fac4081680a74dfa71e561
koda <hogehogehoge@gmail.com>
2019-01-29 21:00:29 +0900

    migrate編集

--- db/migrate/20190125130636_add_user_ref_to_profile.rb
+++ db/migrate/20190125130636_add_user_ref_to_profile.rb
@@ -1,5 +1,5 @@
 class AddUserRefToProfile < ActiveRecord::Migration[5.2]
   def change
-    add_reference :profile, :user, foreign_key: true
+    add_reference :profiles, :user, foreign_key: true
   end
 end
